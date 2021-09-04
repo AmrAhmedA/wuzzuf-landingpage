b9390ce0adaff546a75722b01b7ed1e4f75b6ce5
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2021-09-04 11:29:44 +0200

    Add home file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LandingPage from './component/home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <LandingPage />
     </div>
   );
 }
